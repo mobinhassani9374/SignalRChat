82734271c219e485faa91e5dbfa1c691f924cafa
mobinhassani <mobinhassani9374@gmail.com>
2019-07-18 12:22:42 +0430

    first commit

---
+++ SignalRChat/Hubs/ChatHub.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+
+namespace SignalRChat.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public async Task SendMessage(string name, string message)
+        {
+            await Clients.Others.SendAsync("ReceiveMessage", name, message);
+        }
+
+        public async Task Join(string name)
+        {
+            await Clients.Others.SendAsync("Join", name);
+        }
+    }
+}
---
+++ SignalRChat/wwwroot/lib/chat.js
@@ -0,0 +1,36 @@
+﻿/// <reference path="signalr/dist/browser/signalr.js" />
+var name;
+var connection;
+function saveName() {
+    name = document.getElementById("txtName").value;
+    document.getElementById("intro").style.display = "none";
+    document.getElementById("wellcome").style.display = "block";
+    document.getElementById("sendMessage").style.display = "block";
+    document.getElementById("wellcome-name").innerHTML = name;
+    hubInit();
+}
+function hubInit() {
+
+    connection = new signalR.HubConnectionBuilder().withUrl("/chat").build();
+
+    connection.on("Join", function (name) {
+        document.getElementById("allMessage").innerHTML += "<p style='color:red;'>" + name + " به چت پیوست . </p > ";
+    });
+
+    connection.on("ReceiveMessage", function (name, message) {
+        document.getElementById("allMessage").innerHTML += "<p style='color:green;'>" + name + ": " + message + " </p > ";
+    });
+
+    connection.start().then(function () {
+        connection.invoke("Join", name);
+
+    }).catch(function (err) {
+        alert(err);
+    });
+}
+function send() {
+    var message = document.getElementById("txtMessage").value;
+    document.getElementById("txtMessage").value = "";
+    connection.invoke("SendMessage", name, message);
+    document.getElementById("allMessage").innerHTML += "<p style='color:blue;'>" + name + ": " + message + " </p > ";
+}
